80a4fae510640bcdcee8ce9c6b953e349bdd7912
李熙 <476241029@qq.com>
2021-02-01 15:40:37 +0800

    上传代码

---
+++ 批量注释/OracleCode.cs
@@ -0,0 +1,126 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 批量注释
+{
+    public class OracleCode
+    {
+        private string conectString = string.Empty;
+        private static OracleConnection con = null;
+        private string mTableName = string.Empty;
+
+
+        public void ConnDB()
+        {
+            try
+            {
+                var dbName = ConfigurationManager.AppSettings["dbName"].ToString();
+                if (string.IsNullOrEmpty(dbName))
+                {
+                    Console.WriteLine("请配置好数据库连接信息");
+                    return;
+                }
+                con = new OracleConnection(dbName);
+                con.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+           
+        }
+
+        public List<ColumnInfo> GetTables()
+        {
+            var sql = $@"
+select a.* from user_tab_comments a
+inner join USER_TABLES t
+    on a.table_name = t.table_name
+where a.comments is null";
+            OracleCommand com = new OracleCommand(sql, con);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            var list = new List<ColumnInfo>();
+            OracleDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                var info = new ColumnInfo();
+                info.TableName = reader["table_name"].ToString();
+                info.Comments = reader["comments"].ToString();
+                list.Add(info);
+
+            }
+            reader.Close();
+            con.Close();
+            return list;
+        }
+
+        public int UpdateTable(string tableName, string str)
+        {
+            var sql = $"comment on table {tableName} is '{str} '";
+            OracleCommand com = new OracleCommand(sql, con);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return com.ExecuteNonQuery();
+        }
+
+        public List<ColumnInfo> GetColumnInfo()
+        {
+            string strSQL = $@"SELECT t.table_name,
+       t.column_name,
+       t.comments  FROM USER_COL_COMMENTS   t 
+where t.comments is  null 
+and table_name in (select table_name from USER_TABLES) ";
+            OracleCommand com = new OracleCommand(strSQL, con);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            var list = new List<ColumnInfo>();
+            OracleDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                var info = new ColumnInfo();
+                info.TableName = reader["table_name"].ToString();
+                info.ColumnName = reader["column_name"].ToString();
+                info.Comments = reader["comments"].ToString();
+                list.Add(info);
+
+            }
+            reader.Close();
+            con.Close();
+            return list;
+        }
+
+        public int UpdateColumn(string tableName, string col, string str)
+        {
+            var sql = $"comment on column {tableName}.{col} is '{str} '";
+            OracleCommand com = new OracleCommand(sql, con);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return com.ExecuteNonQuery();
+        }
+    }
+
+    public class ColumnInfo
+    {
+        public string TableName { get; set; }
+        public string ColumnName { get; set; }
+        public string Comments { get; set; }
+    }
+}
---
+++ 批量注释/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 批量注释
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new OracleCode().ConnDB();
+
+            Console.WriteLine("输入1修改字段，输入2修改表");
+            var num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("是否启用百度自动翻译？1:0");
+            var isBd = Convert.ToInt32(Console.ReadLine()) == 1;
+            if (num == 1)
+            {
+                SetColumn(isBd);
+            }
+            if (num == 2)
+            {
+                SetTale(isBd);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void SetTale(bool isBd)
+        {
+            var list = new OracleCode().GetTables();
+            Console.WriteLine($"未注释表记录：{list.Count}条");
+
+            int i = 0;
+            foreach (var item in list)
+            {
+                if (isBd)
+                {
+                    Console.WriteLine("启用百度翻译");
+                    var bd = BaiduFanyi.GetTranslationFromBaiduFanyi(item.TableName);
+                    if (string.IsNullOrEmpty(bd.Error_code))
+                    {
+                        var fy = bd.Trans_result[0].Dst;
+                        Console.WriteLine($"翻译结果：{fy}");
+                        new OracleCode().UpdateTable(item.TableName, fy);
+                        i += 1;
+                        Console.WriteLine($"-----------------{i}.修改成功------------------");
+                        continue;
+                    }
+                    Console.WriteLine($"翻译失败,{bd.Error_code}");
+
+                }
+                Console.WriteLine($"请输入{item.TableName}的注释");
+                var s = Console.ReadLine();
+
+                new OracleCode().UpdateTable(item.TableName, s);
+                i += 1;
+                Console.WriteLine($"-----------------{i}.修改成功------------------");
+            }
+        }
+
+
+        private static void SetColumn(bool isBd)
+        {
+            var list = new OracleCode().GetColumnInfo();
+            Console.WriteLine($"未注释字段：{list.Count}条");
+            var goup = list.GroupBy(x => x.ColumnName).ToList();
+
+
+            foreach (var item in goup)
+            {
+
+                var grlist = list.Where(x => x.ColumnName == item.Key).ToList();
+                if (isBd)
+                {
+                    Console.WriteLine("启用百度翻译");
+                    var bd = BaiduFanyi.GetTranslationFromBaiduFanyi(item.Key);
+                  
+                    if (string.IsNullOrEmpty(bd.Error_code))
+                    {
+                        var fy = bd.Trans_result[0].Dst;
+                        Console.WriteLine($"翻译结果：{fy}");
+                        Zhushi(item, fy, grlist);
+                        continue;
+                    }
+                    Console.WriteLine($"翻译失败,{bd.Error_code}");
+                    continue;
+
+                }
+                if (grlist.Count < 5)
+                {
+                    Zhushi(item, "表字段", grlist);
+                    continue;
+                }
+                Console.WriteLine($"请输入{item.Key}的注释");
+                var s = Console.ReadLine();
+
+                Zhushi(item, s, grlist);
+            }
+        }
+
+        private static void Zhushi(IGrouping<string, ColumnInfo> item, string s, List<ColumnInfo> grlist)
+        {
+            int i = 0;
+            foreach (var a in grlist)
+            {
+                new OracleCode().UpdateColumn(a.TableName, item.Key, s);
+                i += 1;
+            }
+            Console.WriteLine($"修改{i}条数据");
+            Console.WriteLine($"未注释字段：{new OracleCode().GetColumnInfo().Count}条");
+        }
+    }
+}
